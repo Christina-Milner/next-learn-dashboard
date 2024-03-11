c4a0bda7b9a9ba781d6d2631cdb07b86c62eab7f
Christina Milner <christina@christina-milner.dev>
2024-03-11 19:29:32 +0000

    Up to chapter 10

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,39 @@
+
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
+ 
+export default async function Page() {
+  
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
+
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices /> {/* Remove fetch from prop and put it inside component, then wrap with <Suspense> boundary and skeleton as fallback to stream components */}
+        </Suspense>
+      </div>
+    </main>
+  );
+}
+
+/*
+Route groups: Organize files into logical groups without affecting URL path structure.
+Folder in parentheses => not part of the URL.
+Doing this here to ensure loading.tsx only applies to the dashboard overview page, but can also
+be used to separate app into sections.
+*/
